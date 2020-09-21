69742a421a572053f033473d09605556f9dd03c1
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2020-09-21 15:29:15 -0700

    book constructor

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.get('/', renderHomePage);
 // app.get('/hello', testPage);
 app.get('/searchform', renderSearchForm);
-// app.post('/searches', collectFormInformation);
+app.post('/searches', collectFormInformation);
 
 // function testPage(request, response) {
 // 	response.render('pages/index');
@@ -36,33 +36,43 @@ function renderSearchForm(request, response) {
 	response.render('pages/searches/new.ejs');
 }
 
-// function collectFormInformation(request, response) {
-// 	// collect form information
-// 	// console.log(request.body);
-// 	// { search: [ 'Elizabeth Moon', 'author' ] }
-// 	// { search: [ 'The magic of thinking big', 'title' ] }
-// 	const searchQuery = request.body.search[0];
-// 	const searchType = request.body.search[1];
+function collectFormInformation(request, response) {
+	// collect form information
+	// console.log(request.body);
+	// { search: [ 'Elizabeth Moon', 'author' ] }
+	// { search: [ 'The magic of thinking big', 'title' ] }
+	const searchQuery = request.body.search[0];
+	const searchType = request.body.search[1];
 
-// 	let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-// 	if (searchType === 'title') {
-// 		url += `+intitle:${searchQuery}`;
-// 	}
-// 	if (searchType === 'author') {
-// 		url += `+inauthor:${searchQuery}`;
-// 	}
+	let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+	if (searchType === 'title') {
+		url += `+intitle:${searchQuery}`;
+	}
+	if (searchType === 'author') {
+		url += `+inauthor:${searchQuery}`;
+	}
 
-// 	superagent.get(url).then((data) => {
-// 		const bookArray = data.body.items;
-// 		const finalBookArray = bookArray.map((book) => new Book(book.volumeInfo));
+	superagent.get(url).then((data) => {
+		//array of arrays
+		const bookArray = data.body.items;
+		// console.log(bookArray);
+		//a book is an object with key value pairs
+		const finalBookArray = bookArray.map((book) => new Book(book.volumeInfo));
 
-// 		response.render('pages/show', { bananas: finalBookArray });
-// 	});
-// }
+		response.render('pages/searches/show', { bananas: finalBookArray });
+	});
+}
 
-// function Book(book) {
-// 	this.title = book.title;
-// }
+function Book(book) {
+	this.image = book.imageLinks.thumbnail
+		? book.imageLinks.thumbnail
+		: 'https://i.imgur.com/J5LVHEL.jpg';
+	this.title = book.title;
+	this.author = book.authors;
+	this.description = book.description
+		? book.description
+		: 'No description available.';
+}
 
 app.listen(PORT, () => {
 	console.log(`listening on ${PORT}`);
